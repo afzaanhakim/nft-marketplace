b4987fdaddab1d34b3aa567c1bfee17e20bb3455
afzaanhakim <afzaanhakim19@gmail.com>
2021-09-04 12:28:15 -0400

    hardhat config and setup with polygon network keys, linked to metamask

--- nft-mkt/hardhat.config.js
+++ nft-mkt/hardhat.config.js
@@ -1,21 +1,33 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const fs = require('fs')
+const privateKey = fs.readFileSync(".secret").toString()
+const id = process.env.PROJECT_ID
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+console.log(id)
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+
 module.exports = {
+
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${id}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      URL: `https://polygon-mainnet.infura.io/v3/${id}`,
+      accounts: [privateKey]
+    }
+  },
   solidity: "0.8.4",
 };
